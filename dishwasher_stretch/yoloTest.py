b4234cea7cb6287533849817af0982ddbb1bb43d
varadn <varadnev@gmail.com>
2024-09-12 13:48:46 -0400

    initial commit

---
+++ dishwasher_stretch/yoloTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# https://moveit.ai/ros/moveit/events/2021/10/29/rosworld-moveit-workshop.html
+from ultralytics import YOLO
+
+# loads a small pretrained yolo model
+model = YOLO("yolov8s.pt")
+imageName = 'cat_dog.jpg'
+
+results = model.predict(imageName)
+result = results[0]
+
+print('Objects Detected: ' + str(len(result.boxes)))
+
+for box in result.boxes:
+	print("Object type:", box.cls[0].item())
+	print("Coordinates:", box.xyxy[0].tolist())
+	print("Probability:", box.conf[0].item())
+	print("--------------------------------------")
+
